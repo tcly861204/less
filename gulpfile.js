fae732a8bc10b20673da6fc2db96145ff3166a38
tcly861204 <tcly861204@hotmail.com>
2017-07-08 15:48:39 +0800

    update less

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssnext = require('cssnext'),
+    precss = require('precss'),
     cssmin = require('gulp-minify-css');
 
 gulp.task('Less', function() {
+    var processors = [autoprefixer, cssnext, precss];
     gulp.src('style/*.less')
         .pipe(less())
+        .pipe(postcss(processors))
         .pipe(cssmin())
         .pipe(gulp.dest('css/'));
 });
